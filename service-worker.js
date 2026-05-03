7902cd25e1d6482b8e6a750d7f2b275566700afe
nmmouro <32180968+nmmouro@users.noreply.github.com>
2026-05-03 15:28:52 -0300

    Add service worker for caching and offline support

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "painel-frota-v2";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
